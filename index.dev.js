b804734570d7ea9395a79cf59e6d58ff1a628e79
dubisdev <dubisdev@gmail.com>
2022-08-16 19:25:55 +0200

    fix: build on commonjs

--- index.dev.js
+++ index.dev.js
@@ -1,6 +1,8 @@
-import core from "@actions/core";
-import { TwitterApi } from "twitter-api-v2";
-import { uploadImage, getLatestReleaseText } from "./helpers";
+const core = require( "@actions/core")
+const { TwitterApi } = require( "twitter-api-v2")
+const { uploadImage, getLatestReleaseText } = require( "./helpers")
+
+console.log(core)
 
 const IMG_URL =
   "https://github.com/AddToDoist/AddToDoist/blob/main/assets/social-preview.png?raw=true";
@@ -14,12 +16,17 @@ const userClient = new TwitterApi({
   accessSecret: core.getInput("access-token-secret"),
 });
 
-try {
-  const [imageId, releaseText] = await Promise.all([
-    uploadImage(IMG_URL),
-    getLatestReleaseText(RELEASE_URL),
-  ]);
-  await userClient.v2.tweet(releaseText, { media: { media_ids: [imageId] } });
-} catch (err) {
-  core.setFailed(err.message);
+async function main() {
+  try {
+    const [imageId, releaseText] = await Promise.all([
+      uploadImage(IMG_URL),
+      getLatestReleaseText(RELEASE_URL),
+    ]);
+    await userClient.v2.tweet(releaseText, { media: { media_ids: [imageId] } });
+  } catch (err) {
+    core.setFailed(err.message);
+  }
 }
+
+main()
+
